c0389d7db3d4b7ac7f84dbf9624f55cc928e9578
guisouzanl <guisouzanl@hotmail.com>
2019-07-14 02:20:55 -0300

    chore: enviando email

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5478aad428e2a9',
+    pass: '6e946fc090b7e2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (mailchimp)
+ * Mailtrap (DEV)
+ */
